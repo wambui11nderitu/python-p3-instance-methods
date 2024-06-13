a25a4f8b7b1876d8e1672bbf4f0cd711e503051b
Wambui <nderituwambui93@gmail.com>
2024-06-14 00:40:50 +0300

    updating labs

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,18 @@
+
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
 
-    #Instance method definition
-    pass
+    def sit(self):
+        print("The dog is sitting.")
+
+    def bark(self):
+        print("Woof!")
+
+    def roll_over(self):
+        print("The dog is rolling over.")
+
+fido = Dog()
+fido.sit()
+fido.bark()
+fido.roll_over()
